9a38223cbdbd4647634dc6ecf921a9e2b0310364
anjaliVBA <jsmg.anjali1004@gmail.com>
2024-09-28 12:45:48 +0530

    Updated 02 counter project with new changes

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -9,16 +9,17 @@ function App() {
   let [counter, setCounter]=useState(5);//ab isme aata hai ki hum default value kya dena chahte hai true false anjali ansh kuch bhi de sakte hai functions bhi de skte hai
  // let counter=5//iss variable ko hum update kar sakte hai but ye variable kabhi bhi ui mein propagate nahui hopga
   const addValue=()=>{ 
-    
+    if(counter<20){
     //counter= counter+1;//value agar add ho rhi hai mtlab counter bhi increase ho skta hai ;par counter aise update nahi hopga simply usestate hook ko call kar denge jiske andar increment ka message denge
-   setCounter(counter+1)
+   setCounter(counter+1)}
    console.log("clicked", counter)
     // abhi click toh hua but 5 se 6 ni hua
     // problem aa rhi hai ui updation mein .REACT BOLTA HSI UI KE ANDAR KAB KYA AAYEGA YE AAP NI MAI DECIDE KARUNGA.REACT REACTIVE LIBRARY HAI EK VARIABLE UPDATE HOTE HI HAR JAGAH REACT KARTI HAI
     //YEHI KARNE KE LIYE HOOKS HOTE HAI HUMARE JO KI VARIABLE VALUUE MEIN CHANGE LAATE HAI UI MEINex usestate uselayouteffect each hook ka ek special job hota hai ki itna hi kaam hum karenge aise hi jab badi libraries hum banate hai tab hota hai jyada nhi limited functionalities hoti hai modular function banana padta hai ki ek task sirf ek hi function ko assign ho5 kaam hai toh 5 function se karwao kyunki code debug mein kaafi aasani milti hai
   }
   const decValue=()=>{
-    setCounter(counter-1)
+    if(counter>0){
+    setCounter(counter-1)}
     console.log("clicked",counter)
   }
   // ab jaha bhin counter dikhega waha waha increment hoga
